98466635dc1640f670bb603ceee0e8d4986fa92b
Thijs Dregmans <92675694+tdregmans@users.noreply.github.com>
2023-03-22 10:59:03 +0100

    public version

--- bot.cpp
+++ bot.cpp
@@ -1,33 +1,27 @@
 /*
-  bot.ino
-
-  version 3
-
-  CMI-TI 22 TINNES01
-  Student: Thijs Dregmans (1024272)
-  Netwerken & Security (vervolg) opdracht 2
+  bot.ino - Thijs Dregmans (1024272)
   Last edited: 21-03-2023
-
 */
 
-#include <WiFiClientSecure.h>
-#include <PubSubClient.h>
+
+const int   daylightOffset_sec = 3600;
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
 
 #include "secret.h"
-
-
-#include "time.h"
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 0;
-
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include "DHT.h"
+#include "time.h"
+// Pin the DHT is connected to
+#define DHTPIN 2
+// Type of sensor
+#define DHTTYPE DHT11
 
-#define DHTPIN 13     // what pin we're connected to
-#define DHTTYPE DHT11   // DHT 11 
-
+// Init sensor
 DHT dht(DHTPIN, DHTTYPE);
 
+// Init wifi clients
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
@@ -35,6 +29,64 @@ unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
+
+#define ESPTYPE ESP32
+
+
+// coppied from NPT libary provided by Client
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String RecievedText;
+  for (int i = 0; i < length; i++) {
+    RecievedText += (char)payload[i];
+  }
+  Serial.println(RecievedText);
+
+  if (RecievedText == String(MQTT_CLIENT_ID)+": temperatuur") {
+    delay(2000);
+    float t = dht.readTemperature();
+    Serial.print("Temperatuur is ");
+    Serial.println(t);
+    client.publish(MQTT_TOPIC, String(t).c_str());
+  }
+  else if (RecievedText == String(MQTT_CLIENT_ID)+": vochtigheid") {
+    delay(2000);
+    float h = dht.readHumidity();
+    Serial.print("Luchtvochtigheid is ");
+    Serial.println(h);
+    client.publish(MQTT_TOPIC, String(h).c_str());
+  }
+  else if (RecievedText == String(MQTT_CLIENT_ID)+": led:aan") {
+    digitalWrite(LED_BUILTIN, HIGH);
+    client.publish(MQTT_TOPIC, "LED is aan");
+  }
+  else if (RecievedText == String(MQTT_CLIENT_ID)+": led:uit") {
+    digitalWrite(LED_BUILTIN, LOW);
+    client.publish(MQTT_TOPIC, "LED is uit");
+  }
+
+  //  // Switch on the LED if an 1 was received as first character
+  //  if ((char)payload[0] == '1') {
+  //    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  //    // but actually the LED is on; this is because
+  //    // it is active low on the ESP-01)
+  //  } else {
+  //    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  //  }
+
+}
 
 void setup_wifi() {
 
@@ -52,8 +104,10 @@ void setup_wifi() {
     Serial.print(".");
   }
 
+  // connect certificate !!!!!!!!!!
   espClient.setCACert(local_root_ca);
 
+  // BS
   randomSeed(micros());
 
   Serial.println("");
@@ -62,79 +116,14 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  String command;
-  for (int i = 0; i < length; i++) {
-    command += (char)payload[i];
-  }
-  Serial.println(command);
-
-  if (command == String(MQTT_CLIENT_ID)+": temperatuur") {
-    delay(2000);
-    float t = dht.readTemperature();
-    Serial.print("Temperatuur is ");
-    Serial.println(t);
-    String message = "Temperatuur is ";
-    message.concat(String(t));
-    message.concat("ºC");
-    client.publish(MQTT_TOPIC, message.c_str());
-  }
-  else if (command == String(MQTT_CLIENT_ID)+": vochtigheid") {
-    delay(2000);
-    float h = dht.readHumidity();
-    Serial.print("Luchtvochtigheid is ");
-    Serial.println(h);
-    String message = "Luchtvochtigheid is ";
-    message.concat(String(h));
-    message.concat("%");
-    client.publish(MQTT_TOPIC, message.c_str());
-  }
-  else if (command == String(MQTT_CLIENT_ID)+": led:aan") {
-    digitalWrite(LED_BUILTIN, HIGH);
-    client.publish(MQTT_TOPIC, "LED is aan");
-  }
-  else if (command == String(MQTT_CLIENT_ID)+": led:uit") {
-    digitalWrite(LED_BUILTIN, LOW);
-    client.publish(MQTT_TOPIC, "LED is uit");
-  }
-
-
-
-  //  // Switch on the LED if an 1 was received as first character
-  //  if ((char)payload[0] == '1') {
-  //    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  //    // but actually the LED is on; this is because
-  //    // it is active low on the ESP-01)
-  //  } else {
-  //    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  //  }
-
-}
-
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+    Serial.print("MQTT connecting...");
     if (client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASS)) {
       Serial.println("connected");
       
       // Once connected, publish an announcement...
-      String message = String(MQTT_CLIENT_ID);
-      message.concat(" connected with MQTT broker.");
+      String message = "connected with MQTT broker.";
       client.publish(MQTT_TOPIC, message.c_str());
       
       // ... and resubscribe
@@ -150,6 +139,11 @@ void reconnect() {
 }
 
 void setup() {
+  dht.begin();
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+  
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
@@ -157,10 +151,7 @@ void setup() {
   client.setCallback(callback);
 
 
-  dht.begin();
-  //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  
 }
 
 void loop() {
